0044fa68ff2356587795b9679eaa62028e7ff9aa
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-05-14 17:15:12 -0300

    Criando e configurando os controllers LoginController e ProdutoController (passo 6)

---
+++ New-Sys/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using New_Sys.Repositorio;
+using New_Sys.Models;
+
+
+namespace New_Sys.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly LoginRepositorio _loginRepositorio;
+
+        public LoginController(LoginRepositorio loginRepositorio)
+        {
+            _loginRepositorio = loginRepositorio;
+        }
+
+        public IActionResult ChecarUsuario(string email)
+        {
+            var usuario = _loginRepositorio.ObterUsuario(email);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+    }
+}
--- New-Sys/obj/Debug/net8.0/New-Sys.AssemblyInfo.cs
+++ New-Sys/obj/Debug/net8.0/New-Sys.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("New-Sys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97350c295cde67fe0b3afdcc1f970886975189a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c9065238b2ccdd6faddd2ef9829c28c96d59d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("New-Sys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("New-Sys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
